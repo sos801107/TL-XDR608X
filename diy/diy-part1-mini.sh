f1fdfe21dddff627cbf82f27f552a667ecf72ee0
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-02-20 20:26:41 +0800

    Update diy-part1-mini.sh

--- diy/diy-part1-mini.sh
+++ diy/diy-part1-mini.sh
@@ -30,7 +30,10 @@ wget -qO- https://raw.githubusercontent.com/sos801107/TP-Link-TL-XDR6086/refs/he
 #wget -qO- https://raw.githubusercontent.com/sos801107/TP-Link-TL-XDR6086/refs/heads/main/etc/mosdns > files/etc/config/mosdns
 wget -qO- https://raw.githubusercontent.com/sos801107/TP-Link-TL-XDR6086/refs/heads/main/etc/smartdns > files/etc/config/smartdns
 
-#sed -i 's/openwrt\/luci\/luci.git;openwrt-24.10/g' ./feeds.conf.default
-#sed -i '/luci.git;openwrt-24.10/d' ./feeds.conf.default
-sed -i 's/coolsnowwolf\/luci/openwrt\/luci/g' ./feeds.conf.default
-sed -i 's/openwrt-23.05/openwrt-24.10/g' ./feeds.conf.default
+mkdir -p files/etc/opkg
+wget -qO- https://raw.githubusercontent.com/sos801107/TP-Link-TL-XDR6086/refs/heads/main/etc/distfeeds.conf > files/etc/opkg/distfeeds.conf
+mkdir -p files/root
+wget -qO- https://raw.githubusercontent.com/sos801107/TP-Link-TL-XDR6086/refs/heads/main/etc/.profile > files/root/.profile
+#luci openwrt-24.10
+#sed -i 's/coolsnowwolf\/luci/openwet\/luci/g' ./feeds.conf.default
+#sed -i 's/openwrt-23.05/openwrt-24.10/g' ./feeds.conf.default
