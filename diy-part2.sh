8fbba714a6ab14b644a6715ba77346bef920561e
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-02-26 09:38:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -64,7 +64,7 @@ cp -r feeds/small/luci-app-passwall feeds/luci/applications/luci-app-passwall
 cp -r feeds/small/luci-app-openclash feeds/luci/applications/luci-app-openclash
 # istoreos-theme
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/zijieKwok/istoreos-theme/luci-theme-argon feeds/luci/themes/luci-theme-argon
+git clone https://github.com/zijieKwok/istoreos-theme feeds/luci/themes/luci-theme-argon
 #cp -r feeds/kenzo/luci-theme-argon feeds/luci/themes/luci-theme-argon
 
 
