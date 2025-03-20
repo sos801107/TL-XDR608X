05d2bb184e0dbb0b92b79cda4f0cb403a2bdbb78
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-03-21 06:52:20 +0800

    Update diy-part1-mini.sh

--- diy-part1-mini.sh
+++ diy-part1-mini.sh
@@ -17,7 +17,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git turboacc https://github.com/chenmozhijin/turboacc' >> feeds.conf.default
-echo 'src-git shortcutfe https://github.com/chenmozhijin/turboacc/tree/package/shortcut-fe/shortcut-fe' >> feeds.conf.default
+#echo 'src-git shortcutfe https://github.com/chenmozhijin/turboacc/tree/package/shortcut-fe/shortcut-fe' >> feeds.conf.default
 echo 'src-git theme https://github.com/zijieKwok/istoreos-theme' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
