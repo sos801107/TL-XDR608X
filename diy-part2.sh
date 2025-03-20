4ad8be84640caeaecca439494f1874117802854e
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-03-20 22:55:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -64,7 +64,7 @@ cp -r feeds/small/luci-app-passwall feeds/luci/applications/luci-app-passwall
 cp -r feeds/small/luci-app-openclash feeds/luci/applications/luci-app-openclash
 
 #rm -rf feeds/luci/applications/luci-app-turboacc
-cp -r feeds/turboacc/luci-app-turboacc feeds/luci/applications/luci-app-turboacc
+#cp -r feeds/turboacc/luci-app-turboacc feeds/luci/applications/luci-app-turboacc
 
 # istoreos-theme
 rm -rf feeds/luci/themes/luci-theme-argon
