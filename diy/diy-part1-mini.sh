bdc1d8dd55511b3c75355f5cbb432838ccd257af
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-04-01 23:49:42 +0800

    Update diy-part1-mini.sh

--- diy/diy-part1-mini.sh
+++ diy/diy-part1-mini.sh
@@ -15,8 +15,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-#echo 'src-git luci-app-turboacc https://github.com/chenmozhijin/turboacc/tree/luci/luci-app-turboacc' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git luci-app-turboacc https://github.com/chenmozhijin/turboacc/tree/luci/luci-app-turboacc' >> feeds.conf.default
 #echo 'src-git theme https://github.com/zijieKwok/istoreos-theme' >> feeds.conf.default
 echo 'src-git theme https://github.com/sbwml/luci-theme-argon' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
