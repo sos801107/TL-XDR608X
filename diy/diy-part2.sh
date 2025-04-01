56046b804469c8311474990566d07ab0212e7e21
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-04-01 23:13:48 +0800

    Update diy-part2.sh

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -32,7 +32,7 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-turboacc
-git_sparse_clone package https://github.com/chenmozhijin/turboacc turboacc
+#git_sparse_clone package https://github.com/chenmozhijin/turboacc turboacc
 
 
 ##清除默认密码password
