4a62a3c0c3b41eed5b19d5c34ba625e0b0a93cd3
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-05-04 12:31:41 +0800

    Update diy-part2-x86.sh

--- diy/diy-part2-x86.sh
+++ diy/diy-part2-x86.sh
@@ -46,11 +46,11 @@ echo -e "msgstr \"主题设置\"" >> feeds/luci/applications/luci-app-argon-conf
 #sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # 移除要替换的包
-rm -rf feeds/small/{shadowsocksr-libev,shadowsocks-rust,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-wol,luci-app-bypass}
+rm -rf feeds/kiddin9/{shadowsocksr-libev,shadowsocks-rust,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-wol,luci-app-bypass}
 rm -rf feeds/luci/applications/{shadowsocksr-libev,shadowsocks-rust,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-wol,luci-app-bypass,luci-app-argon-config,luci-theme-argon}
 rm -rf feeds/luci/packages/net/{shadowsocksr-libev-ssr-check,shadowsocksr-libev-ssr-local,shadowsocksr-libev-ssr-redir,shadowsocksr-libev-ssr-server,luci-app-argon-config,luci-theme-argon}
-#rm -rf feeds/small/luci-app-ssr-plus
-#rm -rf feeds/small/luci-i18n-ssr-plus-zh-cn
+#rm -rf feeds/kiddin9/luci-app-ssr-plus
+#rm -rf feeds/kiddin9/luci-i18n-ssr-plus-zh-cn
 #rm -rf feeds/luci/applications/luci-app-ssr-plus
 #rm -rf feeds/luci/applications/luci-i18n-ssr-plus-zh-cn
 #rm -rf feeds/luci/applications/luci-app-wol
@@ -66,21 +66,21 @@ rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/dns2socks
 rm -rf feeds/packages/net/dns2tcp
 rm -rf feeds/packages/net/microsocks
-cp -r feeds/small/xray-core feeds/packages/net
-cp -r feeds/small/mosdns feeds/packages/net
-cp -r feeds/small/v2ray-geodata feeds/packages/net
-cp -r feeds/small/v2ray-geoip feeds/packages/net
-cp -r feeds/small/sing-box feeds/packages/net
-cp -r feeds/small/chinadns-ng feeds/packages/net
-cp -r feeds/small/dns2socks feeds/packages/net
-cp -r feeds/small/dns2tcp feeds/packages/net
-cp -r feeds/small/microsocks feeds/packages/net
+cp -r feeds/kiddin9/xray-core feeds/packages/net
+cp -r feeds/kiddin9/mosdns feeds/packages/net
+cp -r feeds/kiddin9/v2ray-geodata feeds/packages/net
+cp -r feeds/kiddin9/v2ray-geoip feeds/packages/net
+cp -r feeds/kiddin9/sing-box feeds/packages/net
+cp -r feeds/kiddin9/chinadns-ng feeds/packages/net
+cp -r feeds/kiddin9/dns2socks feeds/packages/net
+cp -r feeds/kiddin9/dns2tcp feeds/packages/net
+cp -r feeds/kiddin9/microsocks feeds/packages/net
 
 
 ##更新FQ
 rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
-cp -r feeds/small/luci-app-passwall feeds/luci/applications/luci-app-passwall
-cp -r feeds/small/luci-app-openclash feeds/luci/applications/luci-app-openclash
+cp -r feeds/kiddin9/luci-app-passwall feeds/luci/applications/luci-app-passwall
+cp -r feeds/kiddin9/luci-app-openclash feeds/luci/applications/luci-app-openclash
 
 #rm -rf feeds/luci/applications/luci-app-turboacc
 #cp -r feeds/turboacc/luci-app-turboacc feeds/luci/applications/luci-app-turboacc
