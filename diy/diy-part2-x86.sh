79a73d99309939f16fe14f1a93a5fe3cfeed34d2
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-05-23 12:45:14 +0800

    Update diy-part2-x86.sh

--- diy/diy-part2-x86.sh
+++ diy/diy-part2-x86.sh
@@ -95,13 +95,53 @@ cp -r feeds/small/luci-app-openclash feeds/luci/applications/luci-app-openclash
 
 # istoreos-theme
 rm -rf feeds/luci/themes/luci-theme-argon
-cp -r feeds/theme/luci-theme-argon feeds/luci/themes/luci-theme-argon
+rm -rf feeds/third/luci-theme-argon
+rm -rf feeds/theme/luci-theme-argo
 
+#cp -r feeds/theme/luci-theme-argon feeds/luci/themes/luci-theme-argon
+rm -rf feeds/third/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-argon-config
-cp -r feeds/theme/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/theme/luci-app-argon-config
+
+#cp -r feeds/theme/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+        echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
 
 
-#cp -r feeds/kenzo/luci-theme-argon feeds/luci/themes/luci-theme-argon
+git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-theme-argon
+git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-app-argon-config
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
