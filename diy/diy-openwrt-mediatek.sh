8b74d424a989806c0d5351d65c2553bdd3292521
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-10-30 19:52:55 +0800

    Update diy-openwrt-mediatek.sh

--- diy/diy-openwrt-mediatek.sh
+++ diy/diy-openwrt-mediatek.sh
@@ -46,6 +46,7 @@ rm -rf feeds/kenzo/{luci-app-argon-config,luci-theme-argon}
 rm -rf feeds/third/{luci-app-argon-config,luci-theme-argon}
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/turboaccpackage/nft-fullcone
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
