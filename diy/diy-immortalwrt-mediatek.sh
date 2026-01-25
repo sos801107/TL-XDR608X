6ad7b3a5a0d8ba075c63fd91761b40299bc5d98a
sos801107 <108792640+sos801107@users.noreply.github.com>
2026-01-25 20:07:15 +0800

    Update diy-immortalwrt-mediatek.sh

--- diy/diy-immortalwrt-mediatek.sh
+++ diy/diy-immortalwrt-mediatek.sh
@@ -83,8 +83,7 @@ function merge_package() {
     cd "$rootdir"
 }
 
-src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
-src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
+git_sparse_clone main https://github.com/Openwrt-Passwall/openwrt-passwall luci-app-passwall
 
 #git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
 
